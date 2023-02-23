3866849cef6d00d4cf9ffc696c47e8969dfb0cae
执素言 <906813462@qq.com>
2023-02-23 12:03:28 +0800

    具名插槽

---
+++ project_6/src/App.vue
@@ -0,0 +1,68 @@
+<script setup>
+import Category from './components/Category.vue';
+import { reactive } from 'vue';
+// const foods = reactive(['火锅', '烧烤', '小龙虾', '牛排']);
+const games = reactive(['pokemon', 'csgo', 'apex', 'lol']);
+/* const films = reactive([
+  '《建国大业》',
+  '《拆弹专家》',
+  '《无间道》',
+  '《霍元甲》',
+]); */
+</script>
+
+<template>
+  <div class="container">
+    <Category title="美食">
+      <template #center>
+        <img src="https://s3.ax1x.com/2021/01/16/srJlq0.jpg" alt="" />
+      </template>
+      <template #footer>
+        <a href="https://www.bilibili.com">更多美食</a>
+      </template>
+    </Category>
+
+    <Category title="游戏">
+      <template #center>
+        <ul>
+          <li v-for="(g, index) in games" :key="index">{{ g }}</li>
+        </ul>
+      </template>
+      <template #footer>
+        <div class="foot">
+          <a href="https://www.bilibili.com">单机游戏</a>
+          <a href="https://www.bilibili.com">网络游戏</a>
+        </div>
+      </template>
+    </Category>
+
+    <Category title="电影">
+      <template #center>
+        <video
+          controls
+          src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"></video>
+      </template>
+
+      <template #footer>
+        <div class="foot">
+          <a slot="footer" href="https://www.bilibili.com">经典</a>
+          <a slot="footer" href="https://www.bilibili.com">热门</a>
+          <a slot="footer" href="https://www.bilibili.com">推荐</a>
+        </div>
+        <h4>欢迎前来观影</h4>
+      </template>
+    </Category>
+  </div>
+</template>
+
+<style scoped>
+.container,
+.foot {
+  display: flex;
+  justify-content: space-around;
+}
+
+h4 {
+  text-align: center;
+}
+</style>
---
+++ project_6/src/components/Category.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps(['title']);
+</script>
+
+<template>
+  <div class="category">
+    <h3>{{ title }}</h3>
+    <!-- 定义一个插槽 -->
+    <slot name="center">这里是默认值,当没有传递具体结构时会出现</slot>
+    <slot name="footer">这里是默认值,当没有传递具体结构时会出现</slot>
+  </div>
+</template>
+
+<style>
+.category {
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+h3 {
+  text-align: center;
+  background-color: orange;
+}
+video {
+  width: 100%;
+}
+
+img {
+  width: 100%;
+}
+</style>
