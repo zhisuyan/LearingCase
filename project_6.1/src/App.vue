7df8b7321a73df23b7a4d79b84c4762a464eee32
执素言 <906813462@qq.com>
2023-02-23 13:58:54 +0800

    作用域插槽

---
+++ project_6.1/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Category from './components/Category.vue';
+</script>
+
+<template>
+  <div class="container">
+    <Category title="游戏">
+      <template v-slot="gamesData">
+        <ul>
+          <li v-for="(g, index) in gamesData.games" :key="index">{{ g }}</li>
+        </ul>
+      </template>
+    </Category>
+
+    <Category title="游戏">
+      <template #slot2="{ games, text }">
+        <ol>
+          <li v-for="(g, index) in games" :key="index">{{ g }}</li>
+        </ol>
+        <h4>{{ text }}</h4>
+      </template>
+    </Category>
+  </div>
+</template>
+
+<style scoped>
+.container,
+.foot {
+  display: flex;
+  justify-content: space-around;
+}
+
+h4 {
+  text-align: center;
+}
+</style>
---
+++ project_6.1/src/components/Category.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { reactive } from 'vue';
+defineProps(['title']);
+const games = reactive(['pokemon', 'csgo', 'apex', 'lol']);
+</script>
+
+<template>
+  <div class="category">
+    <h3>{{ title }}</h3>
+    <!-- 定义一个插槽 -->
+    <slot :games="games"></slot>
+    <slot :games="games" text="This are some text." name="slot2"></slot>
+  </div>
+</template>
+
+<style>
+.category {
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+h3 {
+  text-align: center;
+  background-color: orange;
+}
+video {
+  width: 100%;
+}
+
+img {
+  width: 100%;
+}
+</style>
